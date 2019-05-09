675eb39041f6dc9a3aa68680a8fd0f012379f39f
李尧 <524177717@qq.com>
2019-05-09 18:00:45 +0800

    6666

--- nuxt.config.js
+++ nuxt.config.js
@@ -98,21 +98,21 @@ module.exports = {
           browsers: ['> 1%', 'last 2 versions', 'not ie <= 8']
         }
       }
-    }
+    },
 
     /*
      ** You can extend webpack config here
      */
-    // extend(config, ctx) {
-    //   // Run ESLint on save
-    //   if (ctx.isDev && ctx.isClient) {
-    //     config.module.rules.push({
-    //       enforce: 'pre',
-    //       test: /\.(js|vue)$/,
-    //       loader: 'eslint-loader',
-    //       exclude: /(node_modules)/
-    //     })
-    //   }
-    // }
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
   }
 }
